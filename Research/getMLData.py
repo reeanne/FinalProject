ba9a98ba131ac568f11533db27a208d2cf2a9144
Paulina Koch <kopalinka@wp.pl>
2015-05-05 23:33:58 +0100

    Trying to get the classification to work.

--- Research/getMLData.py
+++ Research/getMLData.py
@@ -26,19 +26,19 @@ def analyse_track(file, output):
 	with open(outputfile) as datafile:
 		data = json.load(datafile)
 		loudness = data["lowlevel"]["average_loudness"]
-		silence20 = data["lowlevel"]["silence_rate_20dB"]["mean"]
-		silence30 = data["lowlevel"]["silence_rate_30dB"]["mean"]
-		silence60 = data["lowlevel"]["silence_rate_60dB"]["mean"]
+		silence20 = data["lowlevel"]["silence_rate_20dB"]["dmean"]
+		silence30 = data["lowlevel"]["silence_rate_30dB"]["dmean"]
+		silence60 = data["lowlevel"]["silence_rate_60dB"]["dmean"]
+		spectral_centroid = data["lowlevel"]["spectral_centroid"]["dmean"]
 		dynamic_complexity = data["lowlevel"]["dynamic_complexity"]
 		bmp = data["rhythm"]["bpm"]
-		chords_change = data["tonal"]["chords_changes_rate"]
 		chords_key = chords_lookup[data["tonal"]["chords_key"]]
-		chords_scale = data["tonal"]["chords_scale"] == "major"
+		chords_scale = data["tonal"]["chords_scale"] == "major" ? 1 : 0
 		key_key = chords_lookup[data["tonal"]["key_key"]]
-		key_scale = data["tonal"]["key_scale"] == "major"
+		key_scale = data["tonal"]["key_scale"] == "major" ? 1: 0
 
-		output.write(file_name + '\t' + str(loudness) + '\t' + str(silence20) + '\t' + str(silence30) + '\t' \
-						+ str(silence60) + '\t' + str(dynamic_complexity) + '\t' + str(bmp) + '\t' + str(chords_change) + '\t' \
+		output.write(file_name + '\t' + str(loudness) + '\t' + str(silence20) + '\t' + str(silence30) + '\t' + str(silence60) + '\t' \
+						+ str(dynamic_complexity) + '\t' + str(bmp) + '\t'  + str(spectral_centroid) + '\t' + str(chords_change) + '\t' \
 						+ str(chords_scale) + '\t' + str(chords_key) + '\t' + str(key_key) + '\t' + str(key_scale) + " \n")
 
 
--- Research/train_pybrain.py
+++ Research/train_pybrain.py
@@ -1,5 +1,6 @@
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure import TanhLayer
+from pybrain.tools.validation import ModuleValidator, CrossValidator
+from pybrain.structure import SigmoidLayer
 import pybrain.tools.shortcuts as shortcuts
 from pybrain.datasets import SupervisedDataSet
 import sys
@@ -15,23 +16,38 @@ def main():
 	result = numpy.array(x).astype('float')
 	length = len(result)
 	width = len(result[0])
-	ds = SupervisedDataSet(width - 2, 2)
+	ds = SupervisedDataSet(width - 2, 1)
 
 
 	for i, row in enumerate(result):
 	#	if i < length * 2 / 3:
-		ds.addSample(row[:-2], row[-2:])
+		ds.addSample(row[:-2], row[-2])
 		#else:
 	#		test_data.append([row[:-2], row[-2:]])
 
-	net = shortcuts.buildNetwork(width - 2, 4, 2, bias=True, hiddenclass=TanhLayer)
-	trainer = BackpropTrainer(net, ds, verbose=True) # learningrate = 0.9, momentum=0.0, weightdecay=0.0, verbose=True) 
-	print trainer.trainEpochs(epochs=1000)
-
+	net = shortcuts.buildNetwork(width - 2, 5, 1, bias=True, hiddenclass=SigmoidLayer)
+	trainer = BackpropTrainer(net, ds, verbose=True, learningrate = 0.0012, momentum=0.0, weightdecay=0.0) 
+	# print trainer.trainEpochs(epochs=1000)
+	print trainer.trainUntilConvergence()
+	evaluation = ModuleValidator.classificationPerformance(trainer.module, ds)
+	validator = CrossValidator(trainer=trainer, dataset=trainer.ds, n_folds=5, valfunc=evaluation)
+	print(validator.validate())
 
 if __name__ == '__main__':
     #demo()
     main()
 
 
+
+# Total error:  0.636792088407
+
+
+
+# 0.618 
+# net = shortcuts.buildNetwork(width - 2, 4, 2, bias=True, hiddenclass=SigmoidLayer)
+# trainer = BackpropTrainer(net, ds, verbose=True, learningrate = 0.0012, momentum=0.0, weightdecay=0.0) 
+
+# 0.598
+# net = shortcuts.buildNetwork(width - 2, 4, 2, bias=True, hiddenclass=SigmoidLayer)
+# trainer = BackpropTrainer(net, ds, verbose=True, learningrate = 0.0012, momentum=0.001, weightdecay=0.0) 
 
