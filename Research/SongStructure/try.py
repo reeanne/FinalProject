bc63356ba7a9e7c04a91e5fced6c0f92b105425b
Paulina Koch <kopalinka@wp.pl>
2015-05-26 15:34:00 +0100

    Some refactoring.

--- Research/SongStructure/try.py
+++ Research/SongStructure/try.py
@@ -15,7 +15,6 @@ from essentia.standard import *
 from pylab import *
 import logging
 
-in_bound_idxs = None
 
 def median_filter(X, M=8):
     """Median filter along the first axis of the feature matrix X."""
@@ -24,7 +23,7 @@ def median_filter(X, M=8):
     return X
 
 
-def cnmf(S, rank, niter=500, hull=False):
+def cnmf(S, rank, iterations=500, hull=False):
     """(Convex) Non-Negative Matrix Factorization.
     Parameters
     ----------
@@ -32,7 +31,7 @@ def cnmf(S, rank, niter=500, hull=False):
         Features matrix. p row features and N column observations.
     rank: int
         Rank of decomposition
-    niter: int
+    iterations: int
         Number of iterations to be used
     Returns
     -------
@@ -42,11 +41,9 @@ def cnmf(S, rank, niter=500, hull=False):
         Activation matrix (decomposed matrix)
         (s.t. S ~= F * G)
     """
-    if hull:
-        nmf_mdl = pymf.CHNMF(S, num_bases=rank)
-    else:
-        nmf_mdl = pymf.CNMF(S, num_bases=rank)  
-    nmf_mdl.factorize(niter=niter)
+
+    nmf_mdl = pymf.CNMF(S, num_bases=rank)  
+    nmf_mdl.factorize(niter=iterations)
     F = np.asarray(nmf_mdl.W)
     G = np.asarray(nmf_mdl.H)
     return F, G
@@ -58,21 +55,21 @@ def most_frequent(x):
     return np.argmax(np.bincount(x))
 
 
-def compute_labels(X, rank, R, bound_idxs, niter=300):
+def compute_labels(X, rank, median_size, bound_idxs, iterations=300):
     """Computes the labels using the bounds."""
 
     print "Computing Labels."
     try:
-        F, G = cnmf(X, rank, niter=niter, hull=False)
+        F, G = cnmf(X, rank, iterations=iterations)
     except:
         return [1]
 
-    label_frames = filter_activation_matrix(G.T, R)
+    label_frames = filter_activation_matrix(G.T, median_size)
     label_frames = np.asarray(label_frames, dtype=int)
 
     labels = []
-    bound_inters = zip(bound_idxs[:-1], bound_idxs[1:])
-    for bound_inter in bound_inters:
+    bound_intervals = zip(bound_idxs[:-1], bound_idxs[1:])
+    for bound_inter in bound_intervals:
         if bound_inter[1] - bound_inter[0] <= 0:
             labels.append(np.max(label_frames) + 1)
         else:
@@ -82,7 +79,7 @@ def compute_labels(X, rank, R, bound_idxs, niter=300):
     return labels
 
 
-def filter_activation_matrix(G, R):
+def filter_activation_matrix(G, median_size):
     """Filters the activation matrix G, and returns a flattened copy."""
 
     #import pylab as plt
@@ -96,13 +93,12 @@ def filter_activation_matrix(G, R):
     G[max_idx] = idx + 1
 
     G = np.sum(G, axis=1)
-    G = median_filter(G[:, np.newaxis], R)
+    G = median_filter(G[:, np.newaxis], median_size)
 
     return G.flatten()
 
 
-def get_segmentation(X, rank, R, rank_labels, R_labels, niter=300,
-                     bound_idxs=None, in_labels=None):
+def get_segmentation(X, rank, median_size, rank_labels, R_labels, iterations=300, in_labels=None):
     """
     Gets the segmentation (boundaries and labels) from the factorization
     matrices.
@@ -114,10 +110,8 @@ def get_segmentation(X, rank, R, rank_labels, R_labels, niter=300,
         Rank of decomposition
     R: int
         Size of the median filter for activation matrix
-    niter: int
+    iterations: int
         Number of iterations for k-means
-    bound_idxs : list
-        Use previously found boundaries (None to detect them)
     in_labels : np.array()
         List of input labels (None to compute them)
     Returns
@@ -130,23 +124,20 @@ def get_segmentation(X, rank, R, rank_labels, R_labels, niter=300,
 
 
     # Find non filtered boundaries
-    compute_bounds = True if bound_idxs is None else False
     while True:
 
-        if bound_idxs is None:
-            try:
-                F, G = cnmf(X, rank, niter=niter, hull=False)
-            except:
-                print "Bounds failed."
-                return np.empty(0), [1]
+        try:
+            F, G = cnmf(X, rank, iterations=iterations)
+        except:
+            print "Bounds failed."
+            return np.empty(0), [1]
 
-            # Filter G
-            G = filter_activation_matrix(G.T, R)
-            if bound_idxs is None:
-                bound_idxs = np.where(np.diff(G) != 0)[0] + 1
+        # Filter G
+        G = filter_activation_matrix(G.T, median_size)
+        bound_idxs = np.where(np.diff(G) != 0)[0] + 1
 
         # Increase rank if we found too few boundaries
-        if compute_bounds and len(np.unique(bound_idxs)) <= 2:
+        if len(np.unique(bound_idxs)) <= 2:
             rank += 1
             bound_idxs = None
         else:
@@ -157,51 +148,37 @@ def get_segmentation(X, rank, R, rank_labels, R_labels, niter=300,
     bound_idxs = np.asarray(bound_idxs, dtype=int)
     if in_labels is None:
         labels = compute_labels(X, rank_labels, R_labels, bound_idxs,
-                                niter=niter)
+                                iterations=iterations)
     else:
         labels = np.ones(len(bound_idxs) - 1)
 
     return bound_idxs, labels
 
 
-def times_to_intervals(times):
-    """Given a set of times, convert them into intervals.
-    Parameters
-    ----------
-    times: np.array(N)
-        A set of times.
-    Returns
-    -------
-    inters: np.array(N-1, 2)
-        A set of intervals.
-    """
-    return np.asarray(zip(times[:-1], times[1:]))
-
-
-def intervals_to_times(inters):
+def intervals_to_times(intervals):
     """Given a set of intervals, convert them into times.
     Parameters
     ----------
-    inters: np.array(N-1, 2)
+    intervals: np.array(N-1, 2)
         A set of intervals.
     Returns
     -------
     times: np.array(N)
         A set of times.
     """
-    return np.concatenate((inters.flatten()[::2], [inters[-1, -1]]), axis=0)
+    return np.concatenate((intervals.flatten()[::2], [intervals[-1, -1]]), axis=0)
 
 
 def remove_empty_segments(times, labels):
     """Removes empty segments if needed."""
-    inters = times_to_intervals(times)
-    new_inters = []
+    intervals = np.asarray(zip(times[:-1], times[1:]))
+    new_intervals = []
     new_labels = []
-    for inter, label in zip(inters, labels):
+    for inter, label in zip(intervals, labels):
         if inter[0] < inter[1]:
-            new_inters.append(inter)
+            new_intervals.append(inter)
             new_labels.append(label)
-    return intervals_to_times(np.asarray(new_inters)), new_labels
+    return intervals_to_times(np.asarray(new_intervals)), new_labels
 
 
 def process_segmentation_level(est_idxs, est_labels, N, frame_times, dur):
@@ -237,8 +214,6 @@ def process_segmentation_level(est_idxs, est_labels, N, frame_times, dur):
     # Remove empty segments if needed
     est_times, est_labels = remove_empty_segments(est_times, est_labels)
 
-    # Make sure that the first and last times are 0 and duration, respectively
-
     return est_times, est_labels
 
 
@@ -264,36 +239,10 @@ def get_predominant(sampling_rate):
 
     n_frames = len(pitch)
     print "number of frames:", n_frames
-
-    # Visualize output pitch values
-    fig = plt.figure()
-    plot(range(n_frames), pitch, 'b')
-    n_ticks = 10
-    xtick_locs = [i * (n_frames / 10.0) for i in range(n_ticks)]
-    xtick_lbls = [i * (n_frames / 10.0) * hopSize / sampling_rate for i in range(n_ticks)]
-    xtick_lbls = ["%.2f" % round(x,2) for x in xtick_lbls]
-    plt.xticks(xtick_locs, xtick_lbls)
-    ax = fig.add_subplot(111)
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Pitch (Hz)')
-    suptitle("Predominant melody pitch")
-
-    # Visualize output pitch confidence
-    fig = plt.figure()
-    plot(range(n_frames), confidence, 'b')
-    n_ticks = 10
-    xtick_locs = [i * (n_frames / 10.0) for i in range(n_ticks)]
-    xtick_lbls = [i * (n_frames / 10.0) * hopSize / sampling_rate for i in range(n_ticks)]
-    xtick_lbls = ["%.2f" % round(x,2) for x in xtick_lbls]
-    plt.xticks(xtick_locs, xtick_lbls)
-    ax = fig.add_subplot(111)
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Confidence')
-    suptitle("Predominant melody pitch confidence")
     return pitch
 
 
-def extract_features_librosa2():
+def extract_features():
 
     frame_size = 2048
     hop_size = 512
@@ -306,14 +255,16 @@ def extract_features_librosa2():
     print("HPSS.")
     waveform_harmonic, waveform_percussive = librosa.effects.hpss(waveform)
 
-    #plot(waveform[1*44100:2*44100])
-    #show()
     print("Beats.")
     tempo, beats_idx = librosa.beat.beat_track(y=waveform_percussive, sr=sampling_rate, hop_length=hop_size)
     #frame_time = librosa.frames_to_time(beats_idx, sr=sampling_rate, hop_length=hop_size)
 
     print("Melspectrogram.")
-    S = librosa.feature.melspectrogram(waveform, sr=sampling_rate, n_fft=frame_size, hop_length=hop_size, n_mels=n_mels)
+    S = librosa.feature.melspectrogram(waveform,
+                                       sr=sampling_rate,
+                                       n_fft=frame_size,
+                                       hop_length=hop_size,
+                                       n_mels=n_mels)
 
     print("Predominant.")
     pitch = get_predominant(sampling_rate)
@@ -331,11 +282,11 @@ def extract_features_librosa2():
             print(len(hpcp))
     else:
         print("HPCP 2")
-        hpcp = librosa.feature.chroma_cqt(y=waveform_harmonic, sr=sampling_rate, hop_length=hop_size).T
+        hpcp = librosa.feature.chroma_cqt(y=waveform_harmonic, sr=sampling_rate,
+                                          hop_length=hop_size).T
         print(len(hpcp))
         with open('data.json', 'w') as outfile:
             json.dump({"hpcp": hpcp.tolist()}, outfile)
-
 
     print("Beat synchronising.")
     bs_mfcc = librosa.feature.sync(mfcc.T, beats_idx, pad=False).T
@@ -360,11 +311,11 @@ def newfunction():
     n_mels = 128
     mfcc_coeff = 14
     sampling_rate = 11025
-    niter = 500 
+    iterations = 500 
     H = 20
 
     #swapped
-    hpcp, mfcc, beats, dur = extract_features_librosa2()
+    hpcp, mfcc, beats, dur = extract_features()
     hpcp = lognormalise_chroma(hpcp)
 
     if hpcp.shape[0] >= H:
@@ -372,23 +323,19 @@ def newfunction():
         hpcp = median_filter(hpcp, M=20)
         # Find the boundary indices and labels using matrix factorization
         print("Segmentation.")
-        est_idxs, est_labels = get_segmentation(hpcp.T, 3, 16, 4, 16, niter=niter, bound_idxs=in_bound_idxs, in_labels=None)
+        est_idxs, est_labels = get_segmentation(hpcp.T, 3, 16, 4, 16, iterations=iterations, in_labels=None)
         est_idxs = np.unique(np.asarray(est_idxs, dtype=int))
     else:
         # The track is too short. We will only output the first and last
         # time stamps
-        if in_bound_idxs is None:
-            est_idxs = np.array([0, F.shape[0]-1])
-            est_labels = [1]
-        else:
-            est_idxs = in_bound_idxs
-
+        est_idxs = np.array([0, F.shape[0]-1])
+        est_labels = [1]
+  
     # Post process estimations
     est_idxs, est_labels = postprocess(est_idxs, est_labels)
-    frames = librosa.frames_to_time(beats, sr=sampling_rate,
-                                             hop_length=hop_size)
-    est_times, est_labels = process_segmentation_level(
-            est_idxs, est_labels, hpcp.shape[0], frames, dur)
+    frames = librosa.frames_to_time(beats, sr=sampling_rate, hop_length=hop_size)
+    est_times, est_labels = process_segmentation_level(est_idxs, est_labels,
+                                                       hpcp.shape[0], frames, dur)
     print est_times, est_labels
     return est_times, est_labels
 
