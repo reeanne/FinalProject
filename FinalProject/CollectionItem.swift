ea5b2dd06b3e387f757006ca51e59fe383db9539
Paulina Koch <kopalinka@wp.pl>
2015-05-29 00:04:03 +0100

    Grid view itself working, just items initialisation failing.

---
+++ FinalProject/CollectionItem.swift
@@ -0,0 +1,54 @@
+//
+//  CollectionItem.swift
+//  FinalProject
+//
+//  Created by Paulina Koch on 28/05/2015.
+//  Copyright (c) 2015 Paulina Koch. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+
+
+class CollectionItem: NSCollectionViewItem {
+    
+
+    
+    @IBOutlet weak var star1: NSImageView!
+    @IBOutlet weak var star2: NSImageView!
+    @IBOutlet weak var star3: NSImageView!
+    
+    
+    @IBOutlet weak var artwork: NSImageView!
+    @IBOutlet weak var levelName: NSTextField!
+    @IBOutlet weak var score: NSTextField!
+    
+    
+    func showStars(number: Int) {
+        if (number < 3) {
+            star3.hidden = true;
+        }
+        if (number < 2) {
+            star2.hidden = true;
+        }
+        if (number < 1) {
+            star1.hidden = true;
+        }
+    }
+    
+    func showLevelName(title: String) {
+        levelName.stringValue = title;
+    }
+    
+    func showScore(passedScore: Int) {
+        score.stringValue = passedScore.description;
+    }
+    
+    func showArtwork(image: NSImage) {
+        artwork.image = image;
+    }
+    
+    
+    
+}
--- FinalProject/LevelsController.swift
+++ FinalProject/LevelsController.swift
@@ -18,67 +18,42 @@ class LevelsController: NSViewController {
     var userData: User! = nil;
     var filePath: String! = nil;
     var appDelegate: AppDelegate! = nil;
+    var collArray: NSMutableArray! = nil;
+    var collection: NSMutableArray = NSMutableArray();
     
+    
+    @IBOutlet var arrayController: NSArrayController!
     @IBOutlet var theView: NSVisualEffectView!
-    @IBOutlet weak var collection: NSScrollView!
+    @IBOutlet weak var collectionView: NSCollectionView!
+    
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collArray = NSMutableArray();
+
         appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate;
         managedObjectContext = (NSApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext;
         userData = getUser(appDelegate.user.username);
         var levelFiles = getLevelFiles(userData);
-        var musicData: MusicEntry;
-        for levelFile in levelFiles {
-            musicData = getMetaDataForSong(levelFile)!;
+        var item: CollectionItem;
+        for (index, (name, url)) in enumerate(levelFiles) {
+            item = CollectionItem();
+            item.showArtwork(getAlbumArtworkInfo(url));
+            item.showLevelName(name)
+            collArray.insertObject(item, atIndex: index)
         }
 
     }
-    
-    func getMetaDataForSong(fileURL: NSURL) -> MusicEntry? {
-        
-        var result = MusicEntry();
-        var id3DataSize: UInt32 = 0;
-        
-        var audioFileID: AudioFileID = nil;
-        
-        var err = AudioFileOpenURL(fileURL, Int8(kAudioFileReadPermission), 0, &audioFileID)
-    
-        err = AudioFileGetPropertyInfo(audioFileID, UInt32(kAudioFilePropertyID3Tag), &id3DataSize, nil)
-        if err != Int32(noErr) {
-            NSLog("AudioFileGetPropertyInfo failed for id3 tag")
-            return nil;
-        }
-        
-        var dictionary: NSDictionary = NSDictionary()
-        var piDataSize : UInt32 = UInt32(sizeof(NSDictionary))
-        err = AudioFileGetProperty(audioFileID, UInt32(kAudioFilePropertyInfoDictionary), &piDataSize, &dictionary)
 
-        if err != Int32(noErr) {
-            NSLog("AudioFileGetProperty failed for property info dictionary")
-        }
-        var album: AnyObject? = dictionary[NSString(string: kAFInfoDictionary_Album)];
-        var artist: AnyObject? = dictionary[NSString(string: kAFInfoDictionary_Artist)];
-        var title: AnyObject? = dictionary[NSString(string: kAFInfoDictionary_Title)];
-        
-        println(title)
-        if (album != nil) { result.album = album as! String }
-        if (artist != nil) { result.artist = artist as! String }
-        if (title != nil) { result.title = title as! String }
-        var image: NSImage = getAlbumArtworkInfo(fileURL);
-        println(image)
-        return result;
-        
-    }
-    
 
 
     func getAlbumArtworkInfo(fileURL: NSURL) -> NSImage {
 
         var asset: AVURLAsset = AVURLAsset(URL: fileURL, options:nil);
         var artworks = AVMetadataItem.metadataItemsFromArray(asset.commonMetadata, withKey: AVMetadataCommonKeyArtwork, keySpace: AVMetadataKeySpaceCommon)
-        var currentSongArtwork: NSImage = NSImage();
+        var currentSongArtwork: NSImage = NSImage(byReferencingFile: "play.png")!;
         for item in artworks {
             var subitem = item as! AVMetadataItem;
             if (item.keySpace == AVMetadataKeySpaceID3) {
@@ -91,40 +66,19 @@ class LevelsController: NSViewController {
                // var currentSongArtwork = NSImage(data: dp.objectForKey("data") as! NSData);
             } else if (item.keySpace == AVMetadataKeySpaceiTunes) {
                 println("siabababa")
+            } else {
+                currentSongArtwork = NSImage(byReferencingFile: "play.png")!
             }
         }
         return currentSongArtwork;
     }
     
-    func initialiseGridView() {
-        
-    }
-    /*
-    func makeDeleteButtonForCell(cell: NSCollectionViewCellcell) -> CollectionViewCellButton {
-        var button: CollectionViewCellButton  = CollectionViewCellButton(buttonWithType: NSButtonTypeCustom);
-        var newImageSize: CGSize = CGSizeMake(cell.frame.size.width/2.5, cell.frame.size.height/2.5);
-    
-        var width: CGFloat = 100;
-        var height: CGFloat = 100;
-        var X: CGFloat = cell.frame.size.width - width;
-        var Y: CGFloat = cell.frame.origin.y;
-    
-    button.frame = CGRectMake(X, Y, width, height);
-    [button setImage:image forState:UIControlStateNormal];
-    [button addTarget:self
-    action:@selector(deleteCollectionViewCell:)
-    forControlEvents:UIControlEventTouchUpInside];
-    
-    return button;
-    
 
-    }
-    */
     /**
         Retrieves all the levels from the Core Data.
     */
-    func getLevelFiles(owner: User?) -> [NSURL] {
-        var levels: [NSURL] = [];
+    func getLevelFiles(owner: User?) -> [String: NSURL] {
+        var levels: [String: NSURL] = [String : NSURL]();
         let fetchRequest = NSFetchRequest(entityName: "Level")
         let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
         if ((owner) != nil) {
@@ -136,7 +90,7 @@ class LevelsController: NSViewController {
         if let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [Level] {
             for level in fetchResults {
                 var url = NSURL(fileURLWithPath: level.melody.file);
-                levels.append(url!);
+                levels.updateValue(url!, forKey: level.name);
             }
         }
         return levels;
